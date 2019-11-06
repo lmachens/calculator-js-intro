dfeca43b96773d94d0a18ef639d36afee4414879
lmachens <leon.machens@googlemail.com>
2019-11-06 13:48:14 +0100

    add operators

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
+"use strict";
+
+import { sum, sub, multiply, divide } from "./lib/operators";
+
 const firstInput = document.querySelector("[name=first]");
 const secondInput = document.querySelector("[name=second]");
-const submitButton = document.querySelector(".submit");
+const sumButton = document.querySelector(".sum");
+const subButton = document.querySelector(".sub");
+const multiplyButton = document.querySelector(".multiply");
+const divideButton = document.querySelector(".divide");
+const result = document.querySelector("#result");
 
-submitButton.addEventListener("click", function() {
-  const sum = parseInt(firstInput.value) - parseInt(secondInput.value);
-  alert(sum);
+function getInput() {
+  const firstNumber = parseInt(firstInput.value);
+  const secondNumber = parseInt(secondInput.value);
+
+  return {
+    first: firstNumber,
+    second: secondNumber
+  };
+}
+
+sumButton.addEventListener("click", function() {
+  const numbers = getInput();
+  result.innerHTML = sum(numbers.first, numbers.second);
+});
+
+subButton.addEventListener("click", function() {
+  const numbers = getInput();
+  result.innerHTML = sub(numbers.first, numbers.second);
+});
+
+multiplyButton.addEventListener("click", function() {
+  const numbers = getInput();
+  result.innerHTML = multiply(numbers.first, numbers.second);
+});
+
+divideButton.addEventListener("click", function() {
+  const numbers = getInput();
+  result.innerHTML = divide(numbers.first, numbers.second);
 });
---
+++ src/lib/operators.js
@@ -0,0 +1,15 @@
+export function sum(first, second) {
+  return first + second;
+}
+
+export function sub(first, second) {
+  return first - second;
+}
+
+export function multiply(first, second) {
+  return first * second;
+}
+
+export function divide(first, second) {
+  return first / second;
+}
