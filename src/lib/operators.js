ca1819d17076d357645ce876b557551d5700e97d
lmachens <leon.machens@googlemail.com>
2019-11-06 16:54:23 +0100

    add countDigits function

--- src/lib/operators.js
+++ src/lib/operators.js
@@ -13,3 +13,25 @@ export function multiply(first, second) {
 export function divide(first, second) {
   return first / second;
 }
+
+// export function countDigits(number) {
+//   let count = 0;
+//   if (number < 0) {
+//     number = number * -1; // equal to number *= -1
+//     count = count + 1; // equal to ++count or count++
+//   }
+//   if (number >= 0) {
+//     count = count + 1; // equal to ++count or count++
+//   }
+
+//   while (number / 10 >= 1) {
+//     number = number / 10; // equal to number /= 10
+//     count = count + 1; // equal to ++count or count++
+//   }
+
+//   return count;
+// }
+
+export function countDigits(number) {
+  return number.toString().length;
+}
--- src/lib/operators.test.js
+++ src/lib/operators.test.js
@@ -1,4 +1,4 @@
-import { sum } from "./operators";
+import { sum, countDigits } from "./operators";
 
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum(1, 2)).toBe(3);
@@ -16,4 +16,24 @@ test("adds (-1) + (-2) to equal -3", function() {
 test("adds 0 + 0 to equal 0", function() {
   const result = sum(0, 0);
   expect(result).toBe(0);
+});
+
+test("count digits of 213", function() {
+  const result = countDigits(213);
+  expect(result).toBe(3);
+});
+
+test("count digits of 3129823", function() {
+  const result = countDigits(3129823);
+  expect(result).toBe(7);
+});
+
+test("count digits of -231", function() {
+  const result = countDigits(-231);
+  expect(result).toBe(4);
+});
+
+test("count digits of 0", function() {
+  const result = countDigits(0);
+  expect(result).toBe(1);
 });
